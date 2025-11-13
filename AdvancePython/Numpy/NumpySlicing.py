58ca6ed0b53584f7346336d7a247953469fa1ceb
PavanKumarBollu <bollupavankumar12@gmail.com>
2025-11-13 14:40:32 +0530

    Numpy slicing

--- AdvancePython/Numpy/NumpySlicing.py
+++ AdvancePython/Numpy/NumpySlicing.py
@@ -48,21 +48,44 @@ import numpy as n
 
 # numpu slicing [start:stop:step]
 
+# nums = n.array([10,20,30,40,50,5,6,35,47,58,65])
+#
+# print(nums[::]) #print Everything
+# print(nums)#print everything
+# print(nums[4:7]) #print 4,5,6 elements
+# print(nums[::-1]) # print everything but in revers order
+# print(nums[::2]) # print each alternative element
+
+
+
+#slicing multidimensional array [row:column]
+
+# num_multi = n.array(
+#                     [[1,2,3],#0
+#                      #0 1 2 -> index values
+#                      [4,5,6],#1
+#                      #0 1 2 -> index values
+#                      [7,8,9]])#2
+#                      #0 1 2 -> index values
+#
+# # extract the second row of the num_multi array
+# print(num_multi[1,:])
+# # extract the second column of the num_multi array
+# print(num_multi[:,1])
+#
+# print(num_multi[1:]) # gives all the rows staring from 2nd row
+#
+#
+# print( num_multi[[0,2]])
+
+
+#assigning slicing (slicing and assignment)
 nums = n.array([10,20,30,40,50,5,6,35,47,58,65])
-
-print(nums[::]) #print Everything
-print(nums)#print everything
-print(nums[4:7]) #print 4,5,6 elements
-print(nums[::-1]) # print everything but in revers order
-print(nums[::2]) # print each alternative element
-
-
-
-
-
-
-
-
+# 4,5,6 elements replace it with 11
+# 50 , 5 , 6 -- > 11 11 11
+print(nums)
+nums[4:7] = 11
+print(nums)
 
 
 
