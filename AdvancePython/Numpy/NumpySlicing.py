84e7c977a7c6946af954ed9b3b50366d9fddc46d
PavanKumarBollu <bollupavankumar12@gmail.com>
2025-11-13 13:45:45 +0530

    Numpy Indexing

---
+++ AdvancePython/Numpy/NumpySlicing.py
@@ -0,0 +1,30 @@
+#indexing in numpy
+# ni numpy also we can use the indexing concept where each element will be identified with some number
+# we can do the negative indexing also possible
+
+import numpy as n
+
+# names = n.array([  'Alice', 'Bob', 'Charlie',])
+# print(names)
+#
+# #access  the first element of the names array
+# print(names[0])#gives us the first element of the names array
+#
+# # access the last element of the names array
+#
+# print(names[-1]) # gives us the last element of the array
+# print(names[len(names)-1]) # gives us the last element of the array
+#
+
+
+num_multi = n.array(
+                    [[1,2,3],#0
+                     #0 1 2 -> index values
+                     [4,5,6],#1
+                     #0 1 2 -> index values
+                     [7,8,9]])#2
+                     #0 1 2 -> index values
+
+#print the element of 2nd row 3rd column of num_multi array which is exactly 6
+print(f"Element at the 2nd row 3rd position is {num_multi[1, 2]}")
+print(f"Element at the last row last position is {num_multi[len(num_multi)-1, len(num_multi)-1]}")
